2637921877cdcb8d5970f1e4a285521f2dc4d63f
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2023-11-20 14:11:14 -0600

    add data loader implementation

---
+++ deepimpression/data.py
@@ -0,0 +1,17 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+
+
+class CustomDataset(Dataset):
+    def __init__(self, data, labels):
+        self.data = torch.from_numpy(data).float()
+        self.labels = torch.from_numpy(labels).long()
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        x = self.data[index]
+        y = self.labels[index]
+        return x, y
